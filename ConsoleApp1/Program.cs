981513709e4fd394a13e01e91ec340b26a5bd80a
Prva gimnazija <prvabeogim2026@outlook.com>
2026-03-16 12:37:37 +0000

    add tacka class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,11 +6,31 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    class Tacka
+    {
+        public double x;
+        public double y;
+        public Tacka()
+        {
+            x = 0;
+            y = 0;
+        }
+        public Tacka(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        double d()
+        {
+            return Math.Sqrt(x*x + y*y);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             //Filip Konecni
+
         }
     }
 }
